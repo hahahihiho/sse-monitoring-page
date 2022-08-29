98c6a776d5b3daa7683086c40714ed3184c9b93b
hahahihiho <hymy@coinplug.com>
2022-08-29 13:13:14 +0900

    fix : script/test-sample

--- scripts/test-sample.js
+++ scripts/test-sample.js
@@ -1,8 +1,10 @@
-require("dotenv").config();
 const ethers = require("ethers");
+const tx_util = require("../modules/ethers-tx-util");
+
+const INFO = tx_util.configUtils.get_additional_info("contract"); // get info from ./config/contract_info.json
 
 async function getTokens() {
-    let accounts = await hre.ethers.getSigners();
+    // let accounts = await hre.ethers.getSigners();
     const rich = "0xdDf169Bf228e6D6e701180E2e6f290739663a784";
     const to = "0x95bf7e307bc1ab0ba38ae10fc27084bc36fcd605";
     const usdc = "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75";
@@ -11,9 +13,9 @@ async function getTokens() {
         params: [rich],
     });
     let signer = await hre.ethers.getSigner(rich);
-    let token = await hre.ethers.getContractAt("IERC20Metadata", usdc, signer);
+    // let token = await hre.ethers.getContractAt("IERC20Metadata", usdc, signer);
+    const token = new ethers.Contract(INFO.test.usdc.address,INFO.IERC20MetaData.abi,signer);
     // await accounts[0].sendTransaction({to:rich,value:ethers.utils.parseEther("1")})
-
     let decimal = await token.decimals();
     await token.transfer(to, '1'+'0'.repeat(decimal))
     let balance = await token.balanceOf(to);
